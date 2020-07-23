e4cc04f3eceb34bf4ce22a98d89c84eb14004504
harvey magana <heyharv2012@gmail.com>
2020-07-22 18:09:31 -0700

    Fixed the logic of the Lion score board.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [lionScore, setLions] = useState(32);
+  const [lionScore, setLions] = useState(0);
   const [tigerScore, setTigers] = useState(33);
   const [count, setCount] = useState(0);
   const [lionTouchdown, lionSetCount] = useState(0);
@@ -35,12 +35,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => lionSetCount( lionTouchdown + 1 )}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => lionFieldCount( lionFieldGoal + 1 )}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setLions( lionScore + 7 )}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setLions( lionScore + 3 )}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => tigerSetCount( tigerTouchdown + 1 )}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => tigerFieldCount( tigerFieldGoal + 1 )}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => tigerSetCount( tigerTouchdown + 7 )}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => tigerFieldCount( tigerFieldGoal + 3 )}>Away Field Goal</button>
         </div>
       </section>
     </div>
