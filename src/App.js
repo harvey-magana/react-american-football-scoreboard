d17e275c83c85134e3d795b9db7aa29e74048166
harvey magana <heyharv2012@gmail.com>
2020-07-22 18:10:47 -0700

    Fixed the logic of the Tiger score board.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionScore, setLions] = useState(0);
-  const [tigerScore, setTigers] = useState(33);
+  const [tigerScore, setTigers] = useState(0);
   const [count, setCount] = useState(0);
   const [lionTouchdown, lionSetCount] = useState(0);
   const [lionFieldGoal, lionFieldCount] = useState(0);
@@ -39,8 +39,8 @@ function App() {
           <button className="homeButtons__fieldGoal" onClick={() => setLions( lionScore + 3 )}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => tigerSetCount( tigerTouchdown + 7 )}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => tigerFieldCount( tigerFieldGoal + 3 )}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setTigers( tigerScore + 7 )}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setTigers( tigerScore + 3 )}>Away Field Goal</button>
         </div>
       </section>
     </div>
